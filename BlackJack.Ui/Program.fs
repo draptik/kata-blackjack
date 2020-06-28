65d4bbde3ef1a2d8fa73417072519594132011e7
Patrick Drechsler <patrick@pdrechsler.de>
2020-06-28 16:20:12 +0200

    ui: add states and update

--- BlackJack.Ui/Program.fs
+++ BlackJack.Ui/Program.fs
@@ -19,11 +19,73 @@ let printShouldNeverHappen s =
 type PlayerWins = Undefined
 type HouseWins = Undefined
 
+type GameStatus = Started | PlayerPlaying | PlayerBusted | PlayerFinished | DealerPlaying | DealerBusted | DealerFinished
+
 type GameState = {
     Player: Player
     Dealer: Dealer
     Deck: Deck
+    GameStatus: GameStatus
 }
+
+type Action =
+    | PlayerHits
+    | PlayerStays
+    | DealerPlays
+
+let update gameState action =
+    match action with
+    | PlayerHits -> 
+        match (drawCard gameState.Deck) with
+        | None -> gameState
+        | Some (card, d) -> 
+            let newHand = card :: gameState.Player.Hand
+
+            let status = getStatus (gameState.Player.Status, newHand)
+            let gameStatus =
+                match status with
+                | Status.Busted _ -> PlayerBusted
+                | Status.Stayed _ -> PlayerPlaying
+                | _ -> PlayerPlaying
+
+            let p = { 
+                Id = gameState.Player.Id
+                Hand = newHand
+                Status = status 
+            }
+            
+            {
+                Player = p
+                Dealer = gameState.Dealer
+                Deck = d
+                GameStatus = gameStatus
+            }
+    | PlayerStays -> { gameState with GameStatus = PlayerFinished }
+    | DealerPlays ->
+        match (drawCard gameState.Deck) with
+        | None -> gameState
+        | Some (card, d) -> 
+            let newHand = card :: gameState.Dealer.Hand
+
+            let status = getStatus (gameState.Dealer.Status, newHand)
+            let gameStatus =
+                match status with
+                | Status.Busted _ -> DealerBusted
+                | Status.Stayed _ -> DealerPlaying
+                | _ -> DealerPlaying
+
+            let dealer = { 
+                Hand = newHand
+                Status = status 
+            }
+            
+            {
+                Player = gameState.Player
+                Dealer = dealer
+                Deck = d
+                GameStatus = gameStatus
+            }
+
 
 [<EntryPoint>]
 let main argv =
@@ -43,6 +105,7 @@ let main argv =
                 Player = player
                 Dealer = dealer
                 Deck = deckAfterDealerInitialization
+                GameStatus = Started
             }
 
             printfn "player hand: %A" player.Hand
