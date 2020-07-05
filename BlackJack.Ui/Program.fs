005a30f76b625088f6567bc85761c4f960e9eb60
Patrick Drechsler <patrick@pdrechsler.de>
2020-07-05 22:42:51 +0200

    ui: console shows final state (without winner)

--- BlackJack.Ui/Program.fs
+++ BlackJack.Ui/Program.fs
@@ -29,61 +29,6 @@ type Game = {
     GameStatus: GameStatus
 }
 
-// type Action =
-//     | PlayerHits
-//     | PlayerStays
-//     | DealerPlays
-
-// let update gameState action =
-//     match action with
-//     | PlayerHits -> 
-//         match (drawCard gameState.Deck) with
-//         | None -> gameState
-//         | Some (card, deck) -> 
-//             let newHand = card :: gameState.Player.Hand
-//             let status = getStatus (gameState.Player.Status, newHand)
-//             let gameStatus =
-//                 match status with
-//                 | Status.Busted _ -> PlayerBusted
-//                 | Status.Stayed _ -> PlayerPlaying
-//                 | _ -> PlayerPlaying
-//             {
-//                 Player = { Id = gameState.Player.Id; Hand = newHand; Status = status }
-//                 Dealer = gameState.Dealer
-//                 Deck = deck
-//                 GameStatus = gameStatus
-//             }
-//     | PlayerStays -> { gameState with GameStatus = PlayerFinished }
-//     | DealerPlays ->
-//         let dealerResponse = dealerAction { Hand = gameState.Dealer.Hand; Deck = gameState.Deck }
-//         match dealerResponse with
-//         | DealerResponse.DealerError (error, hand, deck) -> 
-//             {
-//                 Player = gameState.Player
-//                 Dealer = { Hand = hand; Status = gameState.Dealer.Status }
-//                 Deck = deck
-//                 GameStatus = DealerError2
-//             }
-//         | DealerResponse.DealerBusted  (score, hand, deck) -> 
-//             {
-//                 Player = gameState.Player
-//                 Dealer = { Hand = hand; Status = Status.Busted score }
-//                 Deck = deck
-//                 GameStatus = DealerBusted
-//             }
-//         | DealerResponse.DealerStayed (score, hand, deck) -> 
-//             {
-//                 Player = gameState.Player
-//                 Dealer = { Hand = hand; Status = Stayed score }
-//                 Deck = deck
-//                 GameStatus = DealerFinished
-//             }
-
-// type PlayerLoopResult = 
-//     | PlayerLoopError 
-//     | PlayerLoopBusted of Score 
-//     | PlayerLoopFinished of (Hand * Deck)
-
 let playerLoop game playerId =
 
     let rec promptPlay handstatusInternal handInternal deckInternal =
@@ -185,20 +130,12 @@ let main argv =
                 GameStatus = Started
             }
 
-            let playerLoopResult = playerLoop initialGameState (PlayerId 1)
-            // match playerLoopResult with
-            // | PlayerLoopBusted score -> printfn "playerResult (busted): %A" score
-            // | PlayerLoopFinished (hand, deckAfterPlayerFinished) -> 
-            //     printfn "playerResult (stayed): %A" hand
+            let gameAfterPlayerFinished = playerLoop initialGameState (PlayerId 1)
+            let gameAfterDealerFinished = dealerTurn gameAfterPlayerFinished
 
-            // | _ -> printf "end"
-            
-            // let result = update initialGameState playerAction
-            // match result.GameStatus with
-            // | DealerFinished -> printfn "dealer finished"
-            // | PlayerFinished -> printfn "player finished"
-            // | _ -> printfn "%A" result.GameStatus
-            
+            printfn "final player hand: %A" gameAfterDealerFinished.Player.Hand
+            printfn "final dealer hand: %A" gameAfterDealerFinished.Dealer.Hand
+
             ()
 
     0 // return an integer exit code
