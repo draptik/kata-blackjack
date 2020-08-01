5e331d14a03c1ed1cf0863c9456f98d7cddb5289
Patrick Drechsler <patrick@pdrechsler.de>
2020-08-01 03:05:20 +0200

    destructuring demo with records and single-case DU

---
+++ BlackJack.Tests/DummyTests.fs
@@ -0,0 +1,43 @@
+module DummyTests
+
+
+open Xunit
+open FsUnit.Xunit
+open BlackJack.Domain
+
+(*
+    'Hand' is a record type:
+        {
+            Cards: HandCards
+            Status: HandStatus
+        }
+
+    'HandCards' is a single-case DU. Which in turn is a 'Card list'.
+    
+    Why the hassle with 'HandCards'?
+    Card games tend to have a deck (a list of cards) and players,
+    which in turn also have a hand (a list of cards).
+*)    
+[<Fact>]
+let ``destructuring cards from a hand. Demo comparing 4 equal patterns`` () =
+
+    let getCards1 (hand: Hand) = match hand.Cards with | HandCards cs -> cs
+    
+    let getCards2 (hand: Hand) =
+        let {Cards = handCards; Status = _} = hand
+        let (HandCards cards) = handCards
+        cards
+        
+    let getCards3 (hand: Hand) =
+        let {Cards = (HandCards cards); Status = _} = hand
+        cards
+
+    let getCards4 {Cards = (HandCards cards); Status = _} = cards
+    
+    let cards = [ {Suit = Spades; Rank = Eight }; {Suit = Hearts; Rank = Queen} ]
+    let hand = { Cards = HandCards cards; Status = CardsDealt }
+
+    hand |> getCards1 |> should equal cards
+    hand |> getCards2 |> should equal cards
+    hand |> getCards3 |> should equal cards
+    hand |> getCards4 |> should equal cards
