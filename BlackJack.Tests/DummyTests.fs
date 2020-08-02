6006dc386a81c8e2843d51768e67cd4cb0b5a8cd
Patrick Drechsler <patrick@pdrechsler.de>
2020-08-02 11:13:04 +0200

    add another destructuring demo

--- BlackJack.Tests/DummyTests.fs
+++ BlackJack.Tests/DummyTests.fs
@@ -19,7 +19,7 @@ open BlackJack.Domain
     which in turn also have a hand (a list of cards).
 *)    
 [<Fact>]
-let ``destructuring cards from a hand. Demo comparing 4 equal patterns`` () =
+let ``destructuring cards from a hand. Demo comparing equal patterns`` () =
 
     let getCards1 (hand: Hand) = match hand.Cards with | HandCards cs -> cs
     
@@ -34,6 +34,10 @@ let ``destructuring cards from a hand. Demo comparing 4 equal patterns`` () =
 
     let getCards4 {Cards = (HandCards cards); Status = _} = cards
     
+    // When dropping `Status = _` from the previous function we have to make the type explicit (`: Hand`),
+    // because there are multiple records containing `Cards`.
+    let getCards5 ({Cards = (HandCards cards)} : Hand) = cards
+    
     let cards = [ {Suit = Spades; Rank = Eight }; {Suit = Hearts; Rank = Queen} ]
     let hand = { Cards = HandCards cards; Status = CardsDealt }
 
@@ -41,3 +45,4 @@ let ``destructuring cards from a hand. Demo comparing 4 equal patterns`` () =
     hand |> getCards2 |> should equal cards
     hand |> getCards3 |> should equal cards
     hand |> getCards4 |> should equal cards
+    hand |> getCards5 |> should equal cards
